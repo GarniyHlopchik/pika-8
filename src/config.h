64b2ec345f90409edbdc9c0d8f051e7540f74d40
GarniyHlopchik <denisbuntovsky@gmail.com>
2026-02-05 18:01:53 +0300

    shader config

--- src/config.h
+++ src/config.h
@@ -1,4 +1,14 @@
 #pragma once
-#include <iostream>
-//glfw3
-//glad
+#include "glad/glad.h"
+#include "iostream"
+#include "GLFW/glfw3.h"
+extern "C" {
+    #include "lua.h"
+    #include "lauxlib.h"
+    #include "lualib.h"
+}
+
+#include "fstream"
+#include "sstream"
+#include "string"
+#include "vector"
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,95 @@
 #include "config.h"
 
+unsigned int make_module(const std::string& filepath, unsigned int module_type);
+unsigned int make_shader(const std::string& vertex_filepath, const std::string& fragment_filepath);
+
 int main(){
-    std::cout << "Hello World!" << std::endl;
+    //opengl setup------------------------
+    GLFWwindow* window;
+
+    if(!glfwInit()){
+        std::cout << "GLFW couldn't start" << std::endl;
+        return -1;
+    }
+    window = glfwCreateWindow(512,512,"PIKA-8",NULL,NULL);
+    glfwMakeContextCurrent(window);
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        glfwDestroyWindow(window);
+        glfwTerminate();
+        return -1;
+    }
+    glViewport(0,0,512,512);
+    //---------------------------------------------------
+    lua_State* L = luaL_newstate();  // Lua vm
+    luaL_openlibs(L);               // setup
+    luaL_dostring(L, "print('hello from lua')"); //just test but it's so beautiful I'm leaving it
+
+    glClearColor(0.0f,1.0f,0.0f,1.0f);
+    unsigned int shader = make_shader(
+        "../src/shaders/vertex.txt",
+        "../src/shaders/fragment.txt"
+    );
+    while(!glfwWindowShouldClose(window)){
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shader);
+        glfwSwapBuffers(window);
+    }
+    glDeleteProgram(shader);
+    glfwDestroyWindow(window);
+    glfwTerminate();
     return 0;
+}
+unsigned int make_shader(const std::string& vertex_filepath, const std::string& fragment_filepath){
+    std::vector<unsigned> modules;
+    modules.push_back(make_module(vertex_filepath, GL_VERTEX_SHADER));
+    modules.push_back(make_module(fragment_filepath, GL_FRAGMENT_SHADER));
+
+    unsigned int shader = glCreateProgram();
+    for(unsigned int shaderModule : modules){
+        glAttachShader(shader, shaderModule);
+    }
+    glLinkProgram(shader);
+    int success;
+    glGetProgramiv(shader, GL_LINK_STATUS, &success);
+    if(!success){
+        char errLog[1024];
+        glGetProgramInfoLog(shader, 1024,NULL,errLog);
+        std::cout << "Shader linking error:\n" << errLog << std::endl;
+
+    }
+    for(unsigned int shaderModule : modules){
+        glDeleteShader(shaderModule);
+    }
+    return shader;
+    }
+unsigned int make_module(const std::string& filepath, unsigned int module_type){
+    std::ifstream file;
+    std::stringstream bufferedLines;
+    std::string line;
+
+    file.open(filepath);
+    while(std::getline(file,line)){
+        bufferedLines << line << "\n";
+    }
+    std::string ShaderSource = bufferedLines.str();
+    const char* ShaderSrc = ShaderSource.c_str();
+    bufferedLines.str("");
+    file.close();
+
+    unsigned int shaderModule = glCreateShader(module_type);
+    glShaderSource(shaderModule,1,&ShaderSrc,NULL);
+    glCompileShader(shaderModule);
+
+    int success;
+    glGetShaderiv(shaderModule, GL_COMPILE_STATUS, &success);
+    if(!success){
+        char errLog[1024];
+        glGetShaderInfoLog(shaderModule, 1024,NULL,errLog);
+        std::cout << "Shader module compilation error:\n" << errLog << std::endl;
+
+    }
+
+    return shaderModule;
 }
