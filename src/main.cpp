62e39dd8004fcd4ce237bfb08fa0762f024f90b1
GarniyHlopchik <denisbuntovsky@gmail.com>
2026-02-11 00:54:08 +0300

    started refactoring

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,13 @@
 #include "config.h"
 #include "sprite_mesh/mesh.h"
-
+#include "shader_utils/shader_utils.h"
 lua_State* L;
 
-unsigned int make_module(const std::string& filepath, unsigned int module_type);
-unsigned int make_shader(const std::string& vertex_filepath, const std::string& fragment_filepath);
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void log_error(const char* err){
+    std::cout << err << std::endl;
+}
 void call_lua(const char* name) {
     lua_getglobal(L, name);
     if (!lua_isfunction(L, -1)) {
@@ -14,6 +16,7 @@ void call_lua(const char* name) {
     }
     if (lua_pcall(L, 0, 0, 0) != LUA_OK) {
         const char* err = lua_tostring(L, -1);
+        log_error(err);
         lua_pop(L, 1);
     }
 }
@@ -31,6 +34,7 @@ void call_lua_update(float delta) {
     // Call: 1 argument, 0 return values
     if (lua_pcall(L, 1, 0, 0) != LUA_OK) {
         const char* err = lua_tostring(L, -1);
+        log_error(err);
         lua_pop(L, 1);
     }
 }
@@ -116,55 +120,3 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }  
-unsigned int make_shader(const std::string& vertex_filepath, const std::string& fragment_filepath){
-    std::vector<unsigned> modules;
-    modules.push_back(make_module(vertex_filepath, GL_VERTEX_SHADER));
-    modules.push_back(make_module(fragment_filepath, GL_FRAGMENT_SHADER));
-
-    unsigned int shader = glCreateProgram();
-    for(unsigned int shaderModule : modules){
-        glAttachShader(shader, shaderModule);
-    }
-    glLinkProgram(shader);
-    int success;
-    glGetProgramiv(shader, GL_LINK_STATUS, &success);
-    if(!success){
-        char errLog[1024];
-        glGetProgramInfoLog(shader, 1024,NULL,errLog);
-        std::cout << "Shader linking error:\n" << errLog << std::endl;
-
-    }
-    for(unsigned int shaderModule : modules){
-        glDeleteShader(shaderModule);
-    }
-    return shader;
-    }
-unsigned int make_module(const std::string& filepath, unsigned int module_type){
-    std::ifstream file;
-    std::stringstream bufferedLines;
-    std::string line;
-
-    file.open(filepath);
-    while(std::getline(file,line)){
-        bufferedLines << line << "\n";
-    }
-    std::string ShaderSource = bufferedLines.str();
-    const char* ShaderSrc = ShaderSource.c_str();
-    bufferedLines.str("");
-    file.close();
-
-    unsigned int shaderModule = glCreateShader(module_type);
-    glShaderSource(shaderModule,1,&ShaderSrc,NULL);
-    glCompileShader(shaderModule);
-
-    int success;
-    glGetShaderiv(shaderModule, GL_COMPILE_STATUS, &success);
-    if(!success){
-        char errLog[1024];
-        glGetShaderInfoLog(shaderModule, 1024,NULL,errLog);
-        std::cout << "Shader module compilation error:\n" << errLog << std::endl;
-
-    }
-
-    return shaderModule;
-}
