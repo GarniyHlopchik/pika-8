a808362c0ff217b9e68b9a53dfeba46df4008167
KotiasFeet <m.kotia678@gmail.com>
2026-02-22 17:21:04 +0200

    fixed text memmory leak

--- src/gfx/gfx.h
+++ src/gfx/gfx.h
@@ -3,8 +3,17 @@
 #include "shader_utils/shader_utils.h"
 #include "sprite_mesh/mesh.h"
 #include "json_reader/get_config.h"
+#include <unordered_map>
+#include <string>
 
-class GFX{
+struct LoadedFont {
+    unsigned int texture_id;
+    unsigned int width;
+    unsigned int height;
+    FontData data;
+};
+
+class GFX {
 public:
     GFX(int w, int h, const char* title);
     ~GFX();
@@ -14,26 +23,18 @@ public:
     void draw(const unsigned int texture, float x, float y, float width, float height, float u1 = 0.0f, float v1 = 0.0f, float u2 = 1.0f, float v2 = 1.0f);    
     void draw_text(const std::string& text, float x, float y, const std::string& font_name = "default",  float scale = 1.0f, float space_multiplier = 0.4f);
     
-
     static GLFWwindow* getWindow();
     static unsigned int getShader();
     void update();
 
-    FontData get_font_data(const std::string& name) {
-        for (const auto& font : config.get_fonts()) {
-            if (font.name == name) {
-                return font;
-            }
-        }
-        throw std::runtime_error("Font not found: " + name);
-    }
+    FontData get_font_data(const std::string& name);
 
 private:
     Config config;
     static GLFWwindow* window;
     SpriteMesh spritemesh;
     static unsigned int shader;
-    unsigned int font_texture;
-    int font_texture_width;
-    int font_texture_height;
+    
+    // Replace font_texture, font_texture_width, etc. with a cache map
+    std::unordered_map<std::string, LoadedFont> font_cache; 
 };
