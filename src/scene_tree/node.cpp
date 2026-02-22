d1694ce78c83d28e9ddfaf81a1e28d13bfdb7036
GarniyHlopchik <denisbuntovsky@gmail.com>
2026-02-23 00:00:17 +0300

    tiny update for lab

--- src/scene_tree/node.cpp
+++ src/scene_tree/node.cpp
@@ -4,15 +4,17 @@ Node::Node(Node* p_parent,int p_id){
     parent = p_parent;
     id = p_id;
 }
-virtual Node::~Node(){
-    for(Node* child : children){
-        remove_child(child);
+Node::~Node(){
+    for (std::vector<Node*>::iterator it = children.begin(); it != children.end(); ++it) {
+        remove_child(*it);
     }
 }
-void add_child(Node* child){
+void Node::add_child(Node* child){
     children.push_back(child);
 }
-void remove_child(Node* child){
+void Node::remove_child(Node* child){
     auto new_end = std::remove(children.begin(), children.end(), child);
     children.erase(new_end, children.end());
 }
+void Node::_init(){}
+void Node::_update(float dt){}
