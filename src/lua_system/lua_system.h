d4af81b2332fb5b439a5dcda2d9a278932201f95
GarniyHlopchik <denisbuntovsky@gmail.com>
2026-02-23 22:19:15 +0300

    expanded lua class

--- src/lua_system/lua_system.h
+++ src/lua_system/lua_system.h
@@ -6,9 +6,14 @@ class LuaSystem{
     LuaSystem();
     ~LuaSystem();
     void load_script(const std::string& path);
+    int load_script_table(const std::string& path);
+    void call_init(int table_ref, int node_id);
+    void call_script_update(float dt);
     void call_update(float dt);
     void call(const char* name);
     void bind_lib(const luaL_Reg* lib, const std::string& name);
+    void remove_table(int id);
     private:
     lua_State* L;
+    std::vector<int> tables;
 };
