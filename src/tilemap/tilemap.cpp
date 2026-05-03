1baeab3e25af697a3cf7e21410357ededd0657da
fntksnd11 <anton.dashuk47@gmail.com>
2026-05-03 14:53:00 +0300

    Add tilemap system files

---
+++ src/tilemap/tilemap.cpp
@@ -0,0 +1,7 @@
+#include "tilemap.h"
+
+namespace tilemap {
+
+
+
+}
---
+++ src/tilemap/tilemap.h
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace tilemap {
+
+    struct Tile {
+        int id = 0;
+    };
+
+    class Tilemap {
+    public:
+        Tilemap() = default;
+        ~Tilemap() = default;
+
+
+    };
+
+}
